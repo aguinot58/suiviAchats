9ae2ffd28e9012ad5416971e919a9c22e2ae35d4
LoloxDev <loris_labarre@outlook.fr>
2022-06-21 10:55:24 +0200

    Ajout d'un acceuil, mise en place bootstrap + nav

---
+++ src/Controller/AccueilController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AccueilController extends AbstractController
+{
+
+    /**
+     * @Route("/", name="home")
+     */
+    public function home(): Response
+    {
+        return $this->render('accueil/home.html.twig');
+    }
+
+
+}
